06b6faa39eaab05b269619a3e61cf385c4918d70
ПК <themorozushka@gmail.com>
2023-03-26 15:47:26 +0300

    hw is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean result = Validator.validate("test_123", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        if (result) {
+            System.out.println("Логин и пароль корректные");
+        } else {
+            System.out.println("Логини и/или пароль некорректный");
+        }
+
     }
 }
